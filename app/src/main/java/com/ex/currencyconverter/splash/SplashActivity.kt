400307c56977b378935fadbcd5b2922a91c17433
Mihail P <mihail.pantuyhin@alseda.by>
2021-02-07 12:03:10 +0300

    add splash

---
+++ app/src/main/java/com/ex/currencyconverter/splash/SplashActivity.kt
@@ -0,0 +1,30 @@
+package com.ex.currencyconverter.splash
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.motion.widget.MotionLayout
+import com.ex.currencyconverter.MainActivity
+import com.ex.currencyconverter.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        findViewById<MotionLayout>(R.id.splashMotion).setTransitionListener(object : MotionLayout.TransitionListener {
+            override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
+                startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            }
+
+            override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) { }
+
+            override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) { }
+
+            override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) { }
+        })
+    }
+
+}
